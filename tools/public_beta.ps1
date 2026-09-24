10f3c2ab9f96211f284495ff02f6084f3dd5fb1e
TheTeu <kingmath3us@gmail.com>
2026-09-24 18:01:14 -0300

    beta: distribute direct local Garage v3

--- tools/public_beta.ps1
+++ tools/public_beta.ps1
@@ -396,6 +396,7 @@ function Update-Beta {
         "tools/campaign_frontend_garage_v1.py",
         "tools/test_frontend_only_garage_v1.ps1",
         "tools/campaign_frontend_garage_v2.py",
+        "tools/campaign_frontend_garage_v3.py",
         "tools/test_frontend_only_v2.ps1",
         "tools/xtea_assets.py",
         "tools/rextreme_economy.py",
